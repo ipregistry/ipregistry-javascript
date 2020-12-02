ipregistry/ipregistry-javascript

commit bafe6e7cfc6180ff0847b1a1cc4ae7079075d7d5

Pass API key using header instead of query parameter

src/request.ts
@@ -167,6 +167,7 @@ export class DefaultRequestHandler implements IpregistryRequestHandler {
 
     protected getAxiosConfig() {
         const headers = {
+            'authorization': `ApiKey ${this.config.apiKey}`,
             'content-type': 'application/json',
         };
 
@@ -217,7 +218,7 @@ export class DefaultRequestHandler implements IpregistryRequestHandler {
     }
 
     protected buildApiUrl(ip: string, options: IpregistryOption[]) {
-        let result = `${this.config.apiUrl}/${ip ? ip : ''}?key=${this.config.apiKey}`;
+        let result = `${this.config.apiUrl}/${ip ? ip : ''}`;
 
         if (options) {
             for (const option of options) {

commit adf51703bbde6556a635057ed9285467b3ed1f61

Fix broken API URL construction when options are used
The bug was introduced by https://github.com/ipregistry/ipregistry-javascript/commit/bafe6e7cfc6180ff0847b1a1cc4ae7079075d7d5

src/request.ts
@@ -221,8 +221,11 @@ export class DefaultRequestHandler implements IpregistryRequestHandler {
         let result = `${this.config.apiUrl}/${ip ? ip : ''}`;
 
         if (options) {
+            let prefix = '?';
+
             for (const option of options) {
-                result += `&${option.name}=${encodeURIComponent(option.value)}`;
+                result += `${prefix}${option.name}=${encodeURIComponent(option.value)}`;
+                prefix = '&';
             }
         }
 