dcc3fd73bfa61d877b442b8ff7e6c27ee8ccff35
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2021-07-26 23:51:47 +0200

    Enhance utility method checking if a user-agent is a bot/spider/crawler

--- src/util.ts
+++ src/util.ts
@@ -18,7 +18,10 @@ export class UserAgent {
 
     public static isBot(userAgent: string): boolean {
         const lowerCaseUserAgent = userAgent.toLowerCase();
-        return lowerCaseUserAgent.includes('spider') || lowerCaseUserAgent.includes('bot');
+        
+        return lowerCaseUserAgent.includes('bot') ||
+            lowerCaseUserAgent.includes('spider') ||
+            lowerCaseUserAgent.includes('slurp');
     }
 
 }
